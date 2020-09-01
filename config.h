dc60694d1e62cdc701b15bb66e10dcefb507ebb9
benibus <bpharks@gmx.com>
2020-09-01 00:51:25 -0400

    Initial config

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", 		"mod_hardware",		5,			2},
+	{"", 		"mod_volume",		30,			8},
+	{"", 		"mod_battery",		10,			7},
+	{"",		"mod_internet",		10,			9},
+	{"", 		"mod_clock",		10,			1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
