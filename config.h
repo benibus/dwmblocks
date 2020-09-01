53657485bf721383072aa746253dcd885d1c0dd2
benibus <bpharks@gmx.com>
2020-09-01 02:24:30 -0400

    config signums

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 		"mod_hardware",		5,			2},
+	{"", 		"mod_hardware",		5,			0},
 	{"", 		"mod_volume",		30,			8},
 	{"", 		"mod_battery",		10,			7},
 	{"",		"mod_internet",		10,			9},
-	{"", 		"mod_clock",		10,			1},
+	{"", 		"mod_clock",		10,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
