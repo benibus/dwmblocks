ac3fcd20ca2ec9bb864fb09e5cba4b2796a7ba7f
benibus <bpharks@gmx.com>
2020-09-16 14:29:38 -0400

    fixed fall-through click actions, changed script names and signums

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Icon         Command                 Interval        Signal */
-	{  "",          "mod_hardware",         5,              0      },
-	{  "",          "mod_volume",           30,             8      },
-	{  "",          "mod_battery",          10,             7      },
-	{  "",          "mod_internet",         10,             9      },
-	{  "",          "mod_clock",            10,             0      },
-	{  "",          "mod_kernel",           60,             0      },
+	{  "",          "mod_sysinfo",          5,              6      },
+	{  "",          "mod_volume",           30,             5      },
+	{  "",          "mod_battery",          10,             4      },
+	{  "",          "mod_network",          10,             3      },
+	{  "",          "mod_date",             10,             2      },
+	{  "",          "mod_kernel",           60,             1      },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
